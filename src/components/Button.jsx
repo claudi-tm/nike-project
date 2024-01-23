c5b2d8e1a21556277d19366f6168b26642fca7fa
Claudio Tomas <ccaducarlos@gmail.com>
2024-01-23 23:35:44 +0200

    feat: added special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,20 @@
-export default function Button({ iconURL, label }) {
+export default function Button({ iconURL, label, type }) {
 	return (
-		<button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+		<button
+			className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+				type === "primary"
+					? "bg-coral-red text-white border-coral-red"
+					: "bg-transparent text-slate-gray  border-slate-gray"
+			}  `}
+		>
 			{label}{" "}
-			{iconURL && <img
-				src={iconURL}
-				alt="arrow_left"
-				className="ml-2 rounded-full w-5 h-5"
-			/>}
-			
+			{iconURL && (
+				<img
+					src={iconURL}
+					alt="arrow_left"
+					className="ml-2 rounded-full w-5 h-5"
+				/>
+			)}
 		</button>
 	);
 }
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,4 +1,42 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
 export default function SpecialOffer() {
-    return (
-    <div>SpecialOffer</div>
-)}
+	return (
+		<section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10  max-container ">
+			<div className="flex-1 ">
+				<img
+					src={offer}
+					alt="offer"
+					width={773}
+					height={687}
+					className="object-contain w-full"
+				/>
+			</div>
+			<div className="flex flex-1 flex-col">
+				<h2 className="text-4xl font-bold ">
+					<span className="text-coral-red">Special</span> Offer
+				</h2>
+				<p className="font-montserrat text-lg leading-normal text-slate-gray mt-5">
+					Embark on a shopping journey that redefines your experience
+					with unbeatable deals. From premier selections to incredible
+					savings, we offer unparalled value that sets us apart
+				</p>
+				<p className="font-montserrat text-lg leading-normal text-slate-gray mt-5">
+					Navivate a real of possibilities designed to fulfill your
+					unique desires, surpassing the loftiest expectations. Your
+					journey with us is nothing short of exceptional
+				</p>
+				<div className="flex flex-wrap gap-4 mt-11">
+					<Button
+						label={"Shop now"}
+						type={"primary"}
+						iconURL={arrowRight}
+					/>
+					<Button label={"Learn more"} />
+				</div>
+			</div>
+		</section>
+	);
+}
